9f1dafeb571ba16184564e445b55e2fcd4625a05
quentindrouet <quentin.drouet109@gmail.com>
2023-11-20 11:27:59 +0100

    Initial Commit

---
+++ drouet-ios/drouet_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  drouet_iosApp.swift
+//  drouet-ios
+//
+//  Created by Quentin Drouet on 20/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct drouet_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
